eb3bc11191a97cea3eedb35124e1f8cc4c71b94e
Nalbertholi <123516681+Nalbertholi@users.noreply.github.com>
2023-08-02 21:43:49 -0300

    Criação da classe BLL
    Criação do método Inserir
    Criação do método Buscar todos
    Criação do método Buscar por id
    Criação do método Buscar por nome
    Criação do método Alterar
    Criação do método Excluir

---
+++ GestaoImobiliaria/BLL/CorretorBLL.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CorretorBLL
+    {
+        public void inserir(Corretor _corretor)
+        {
+            new CorretorDAL().Inserir(_corretor);
+        }   
+        public list<Corretor> BuscarTodos()
+        {
+            return new CorretorDAL().BuscarTodos();
+        }
+        public Corretor BuscarPorId(int _id)
+        {
+            return new CorretorDAL().BuscarPorId(_id);
+        }
+        public List<Corretor> BuscarPorNome(string _nome)
+        {
+            return new CorretorDAL().BuscarPorNome(_nome);
+        }
+        public void Alterar(Corretor _corretor)
+        {
+            new CorretorDAL().Alterar(_corretor);
+        }
+        public void Excluir(int _id)
+        {
+            new CorretorDAL().Excluir(_id);
+        }
+    }
+}
