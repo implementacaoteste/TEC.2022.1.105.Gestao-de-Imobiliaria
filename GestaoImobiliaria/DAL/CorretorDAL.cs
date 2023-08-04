9b8e35327b52e1489cf4312adfa60253f6f7a6b5
Lucaswasd <lucas.queiroz@aluno.senai.br>
2023-08-03 21:57:55 -0300

    rapaz
    acho que foi um ponto e virgula

--- GestaoImobiliaria/DAL/CorretorDAL.cs
+++ GestaoImobiliaria/DAL/CorretorDAL.cs
@@ -93,7 +93,7 @@ namespace DAL
                 cn.Close();
             }
         }
-        
+
 
 
 
@@ -228,43 +228,43 @@ namespace DAL
             {
                 cn.Close();
             }
-            }
-            public Corretor BuscarPorCPF(string _cPF)
+        }
+        public Corretor BuscarPorCPF(string _cPF)
+        {
+            Corretor corretor = new Corretor();
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
             {
-                Corretor corretor = new Corretor();
-                SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
-                try
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id, Nome, NomeCorretor, Email, CPF, Ativo, Senha FROM Corretor WHERE CPF = @CPF";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@CPF", _cPF);
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
                 {
-                    SqlCommand cmd = new SqlCommand();
-                    cmd.Connection = cn;
-                    cmd.CommandText = "SELECT Id, Nome, NomeCorretor, Email, CPF, Ativo, Senha FROM Corretor WHERE CPF = @CPF";
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    cmd.Parameters.AddWithValue("@CPF", _cPF);
-                    cn.Open();
-                    using (SqlDataReader rd = cmd.ExecuteReader())
+                    if (rd.Read())
                     {
-                        if (rd.Read())
-                        {
-                            corretor.Id = Convert.ToInt32(rd["Id"]);
-                            corretor.Nome = rd["Nome"].ToString();
-                            corretor.NomeCorretor = rd["NomeCorretor"].ToString();
-                            corretor.Email = rd["Email"].ToString();
-                            corretor.CPF = rd["CPF"].ToString();
-                            corretor.Ativo = Convert.ToBoolean(rd["Ativo"].ToString());
-                            corretor.Senha = rd["Senha"].ToString();
-                            corretor.GrupoCorretors = new GrupoCorretorDAL().BuscarPorIdCorretor(corretor.Id);
-                        }
+                        corretor.Id = Convert.ToInt32(rd["Id"]);
+                        corretor.Nome = rd["Nome"].ToString();
+                        corretor.NomeCorretor = rd["NomeCorretor"].ToString();
+                        corretor.Email = rd["Email"].ToString();
+                        corretor.CPF = rd["CPF"].ToString();
+                        corretor.Ativo = Convert.ToBoolean(rd["Ativo"].ToString());
+                        corretor.Senha = rd["Senha"].ToString();
+                        corretor.GrupoCorretors = new GrupoCorretorDAL().BuscarPorIdCorretor(corretor.Id);
                     }
-                    return corretor;
                 }
-                catch (Exception ex)
-                {
-                    //adicionar um id de error que nao esta sendo utilizado
-                    //throw new Exception("ocorreu um erro ao tentar buscar id do usuário do banco de dados", ex);
-                }
-                finally
-                {
-                    cn.Close();
-                }
+                return corretor;
+            }
+            catch (Exception ex)
+            {
+                //adicionar um id de error que nao esta sendo utilizado
+                //throw new Exception("ocorreu um erro ao tentar buscar id do usuário do banco de dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
     }
