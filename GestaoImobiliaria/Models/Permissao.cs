c2c7c87fb477fb108dd11fe483784c2f1b4b76cd
Nalbertholi <123516681+Nalbertholi@users.noreply.github.com>
2023-08-30 19:33:31 -0300

    update classe
    Update classe permissao removendo list grupo usuario

--- GestaoImobiliaria/Models/Permissao.cs
+++ GestaoImobiliaria/Models/Permissao.cs
@@ -5,7 +5,6 @@ namespace Models
     public class Permissao
     {
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public List<GrupoUsuario> Grupos { get; set; }
+        public string Descricao { get; set; } 
     }
 }
