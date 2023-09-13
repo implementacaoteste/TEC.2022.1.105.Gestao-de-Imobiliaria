008e6742fbdf2974b28313b36ab312b5deb29fd7
Nalbertholi <123516681+Nalbertholi@users.noreply.github.com>
2023-09-13 20:00:53 -0300

    reserva
    Criando e concertando o button alterar

--- GestaoImobiliaria/DAL/ReservaDAL.cs
+++ GestaoImobiliaria/DAL/ReservaDAL.cs
@@ -17,13 +17,13 @@ namespace DAL
             try
             {
                 SqlCommand cmd = cn.CreateCommand();
-                cmd.CommandText = @"INSERT INTO Reserva(StatusReserva, Prazo, DataReserva) 
-                                    VALUES (@StatusReserva, @Prazo, @DataReserva)";
+                cmd.CommandText = @"INSERT INTO Reservas(IDTerreno, IDCorretor, StatusReserva, Prazo, DataReserva) 
+                                    VALUES (@IDTerreno, @IDCorretor, @StatusReserva, @Prazo, CONVERT(DATETIME, CONVERT(VARCHAR, GETDATE(),107)))";
                 cmd.CommandType = System.Data.CommandType.Text;
                 cmd.Parameters.AddWithValue("@StatusReserva", _reserva.StatusDaReserva);
                 cmd.Parameters.AddWithValue("@Prazo", _reserva.Prazo);
-                cmd.Parameters.AddWithValue("@DataReserva", _reserva.DataReserva);
-
+                cmd.Parameters.AddWithValue("@IDCorretor", _reserva.Corretor.IdCorretor);
+                cmd.Parameters.AddWithValue("@IDTerreno", _reserva.Terreno.IdTerreno);
                 cmd.Connection = cn;
                 cn.Open();
 
@@ -47,7 +47,7 @@ namespace DAL
             {
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = cn;
-                cmd.CommandText = @"SELECT IdReserva,StatusDaReserva, Prazo, DataReserva, PrazoDeRenovacao FROM Reserva";
+                cmd.CommandText = @"SELECT IdReserva, IDCorretor, IDTerreno,StatusReserva, Prazo, DataReserva FROM Reservas";
                 cmd.CommandType = System.Data.CommandType.Text;
 
                 cn.Open();
@@ -57,6 +57,11 @@ namespace DAL
                     {
                         reserva = new Reserva();
                         reserva.IdReserva = (int)rd["IdReserva"];
+                        reserva.Corretor = new CorretorDAL().BuscarPorId((int)rd["IDCorretor"]);
+                        reserva.Terreno = new TerrenoDAL().BuscarPorId((int)rd["IDTerreno"]);
+                        reserva.StatusDaReserva = rd["StatusReserva"].ToString();
+                        reserva.Prazo = Convert.ToDateTime(rd["Prazo"]);
+                        reserva.DataReserva = Convert.ToDateTime(rd["DataReserva"]);
 
 
                         reservalist.Add(reserva);
@@ -72,16 +77,16 @@ namespace DAL
             {
                 cn.Close();
             }
-        } 
+        }
         public Reserva BuscarPorId(int _id)
         {
-            Reserva reserva  = new Reserva();
+            Reserva reserva = new Reserva();
             SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
             try
             {
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = cn;
-                cmd.CommandText = @"SELECT IdReserva,StatusDaReserva, Prazo, DataReserva, PrazoDeRenovacaoFROM Reserva WHERE IdReserva = @Id";
+                cmd.CommandText = @"SELECT IdReserva,StatusReserva, Prazo, DataReserva, IDTerreno, IDCorretor FROM Reservas WHERE IdReserva = @Id";
 
                 cmd.CommandType = System.Data.CommandType.Text;
                 cmd.Parameters.AddWithValue("@Id", _id);
@@ -89,24 +94,26 @@ namespace DAL
                 cn.Open();
                 using (SqlDataReader rd = cmd.ExecuteReader())
                 {
-                    while (rd.Read())
+                    if (rd.Read())
                     {
                         reserva.IdReserva = (int)rd["IdReserva"];
-                        reserva.StatusDaReserva = (string)rd["StatusDaReserva"];
+                        reserva.StatusDaReserva = (string)rd["StatusReserva"];
                         reserva.Prazo = (DateTime)rd["Prazo"];
                         reserva.DataReserva = (DateTime)rd["DataReserva"];
+                        reserva.Corretor = new CorretorDAL().BuscarPorId(Convert.ToInt32(rd["IDCorretor"]));
+                        reserva.Terreno= new TerrenoDAL().BuscarPorId(Convert.ToInt32(rd["IDTerreno"]));
                     }
                 }
-            return reserva;
+                return reserva;
             }
-                catch (Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception("Ocorreu um erro ao tentar buscar Reserva por id no banco de dados", ex) { Data = { { "Id", 30 } } };
             }
             finally
             {
                 cn.Close();
-            }             
+            }
         }
         public void Alterar(Reserva _reserva)
         {
@@ -115,16 +122,23 @@ namespace DAL
             try
             {
                 SqlCommand cmd = cn.CreateCommand();
-                cmd.CommandText = @"UPDATE Reserva SET 
-                                        StatusDaReserva = @StatusDaReserva,
+                cmd.CommandText = @"UPDATE Reservas SET 
+                                        StatusReserva = @StatusReserva,
                                         Prazo = @Prazo,
                                         DataReserva = @DataReserva,
+                                        IDCorretor = @IDCorretor,
+                                        IDTerreno = @IDTerreno
+                                        
                                     WHERE IdReserva = @IdReserva";
 
                 cmd.CommandType = System.Data.CommandType.Text;
 
-                cmd.Parameters.AddWithValue("@Id", _reserva.IdReserva);
-
+                cmd.Parameters.AddWithValue("@IdReserva", _reserva.IdReserva);
+                cmd.Parameters.AddWithValue("@StatusReserva", _reserva.StatusDaReserva);
+                cmd.Parameters.AddWithValue("@Prazo", _reserva.Prazo);
+                cmd.Parameters.AddWithValue("@DataReserva", _reserva.DataReserva);
+                cmd.Parameters.AddWithValue("@IDTerreno", _reserva.Terreno.IdTerreno);
+                cmd.Parameters.AddWithValue("@IDCorretor", _reserva.Corretor.IdCorretor);
 
                 cmd.Connection = cn;
                 cn.Open();
@@ -147,7 +161,7 @@ namespace DAL
             try
             {
                 SqlCommand cmd = cn.CreateCommand();
-                cmd.CommandText = @"DELETE FROM Reserva WHERE IdReserva = @IdReserva";
+                cmd.CommandText = @"DELETE FROM Reservas WHERE IdReserva = @IdReserva";
                 cmd.CommandType = System.Data.CommandType.Text;
 
                 cmd.Parameters.AddWithValue("@IdReserva", _id);
