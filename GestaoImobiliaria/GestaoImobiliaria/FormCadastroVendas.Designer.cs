7bf2ba35aaec10fd16f3dc36c9357908fea4e6df
CristianoJuniorr <124282206+CristianoJuniorr@users.noreply.github.com>
2023-09-28 21:33:15 -0300

    Formulário cadastro de vendas
    Atualização de imagem de fundo e textos.

--- GestaoImobiliaria/GestaoImobiliaria/FormCadastroVendas.Designer.cs
+++ GestaoImobiliaria/GestaoImobiliaria/FormCadastroVendas.Designer.cs
@@ -32,6 +32,7 @@
             System.Windows.Forms.Label iDCorretorLabel;
             System.Windows.Forms.Label Terreno;
             System.Windows.Forms.Label nomeLabel;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastroVendas));
             this.labelCadastroVendas = new System.Windows.Forms.Label();
             this.buttonSalvarVenda = new System.Windows.Forms.Button();
             this.buttonCancelarCadVenda = new System.Windows.Forms.Button();
@@ -42,16 +43,19 @@
             this.nomeTextBox = new System.Windows.Forms.TextBox();
             this.buttonBuscarCorretor = new System.Windows.Forms.Button();
             this.buttonBuscarTerreno = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             iDCorretorLabel = new System.Windows.Forms.Label();
             Terreno = new System.Windows.Forms.Label();
             nomeLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.vendasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // iDCorretorLabel
             // 
             iDCorretorLabel.AutoSize = true;
-            iDCorretorLabel.Location = new System.Drawing.Point(11, 172);
+            iDCorretorLabel.BackColor = System.Drawing.Color.Gold;
+            iDCorretorLabel.Location = new System.Drawing.Point(298, 84);
             iDCorretorLabel.Name = "iDCorretorLabel";
             iDCorretorLabel.Size = new System.Drawing.Size(55, 16);
             iDCorretorLabel.TabIndex = 7;
@@ -60,34 +64,37 @@
             // Terreno
             // 
             Terreno.AutoSize = true;
-            Terreno.Location = new System.Drawing.Point(352, 99);
+            Terreno.BackColor = System.Drawing.Color.Gold;
+            Terreno.Location = new System.Drawing.Point(43, 146);
             Terreno.Name = "Terreno";
-            Terreno.Size = new System.Drawing.Size(71, 16);
+            Terreno.Size = new System.Drawing.Size(55, 16);
             Terreno.TabIndex = 8;
-            Terreno.Text = "IDTerreno:";
+            Terreno.Text = "Terreno";
             // 
             // nomeLabel
             // 
             nomeLabel.AutoSize = true;
-            nomeLabel.Location = new System.Drawing.Point(31, 96);
+            nomeLabel.BackColor = System.Drawing.Color.Gold;
+            nomeLabel.Location = new System.Drawing.Point(47, 84);
             nomeLabel.Name = "nomeLabel";
-            nomeLabel.Size = new System.Drawing.Size(51, 16);
+            nomeLabel.Size = new System.Drawing.Size(48, 16);
             nomeLabel.TabIndex = 17;
-            nomeLabel.Text = "Cliente:";
+            nomeLabel.Text = "Cliente";
             // 
             // labelCadastroVendas
             // 
             this.labelCadastroVendas.AutoSize = true;
+            this.labelCadastroVendas.BackColor = System.Drawing.Color.Gold;
             this.labelCadastroVendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCadastroVendas.Location = new System.Drawing.Point(80, 9);
+            this.labelCadastroVendas.Location = new System.Drawing.Point(122, 17);
             this.labelCadastroVendas.Name = "labelCadastroVendas";
-            this.labelCadastroVendas.Size = new System.Drawing.Size(487, 46);
+            this.labelCadastroVendas.Size = new System.Drawing.Size(375, 46);
             this.labelCadastroVendas.TabIndex = 10;
-            this.labelCadastroVendas.Text = "CADASTRO DE VENDAS";
+            this.labelCadastroVendas.Text = "Cadastro de vendas";
             // 
             // buttonSalvarVenda
             // 
-            this.buttonSalvarVenda.Location = new System.Drawing.Point(411, 172);
+            this.buttonSalvarVenda.Location = new System.Drawing.Point(362, 217);
             this.buttonSalvarVenda.Name = "buttonSalvarVenda";
             this.buttonSalvarVenda.Size = new System.Drawing.Size(75, 23);
             this.buttonSalvarVenda.TabIndex = 11;
@@ -98,7 +105,7 @@
             // buttonCancelarCadVenda
             // 
             this.buttonCancelarCadVenda.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancelarCadVenda.Location = new System.Drawing.Point(555, 172);
+            this.buttonCancelarCadVenda.Location = new System.Drawing.Point(443, 217);
             this.buttonCancelarCadVenda.Name = "buttonCancelarCadVenda";
             this.buttonCancelarCadVenda.Size = new System.Drawing.Size(75, 23);
             this.buttonCancelarCadVenda.TabIndex = 12;
@@ -108,9 +115,9 @@
             // textBox1
             // 
             this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.vendasBindingSource, "NomeCorretor", true));
-            this.textBox1.Location = new System.Drawing.Point(88, 172);
+            this.textBox1.Location = new System.Drawing.Point(301, 104);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(166, 22);
+            this.textBox1.Size = new System.Drawing.Size(182, 22);
             this.textBox1.TabIndex = 14;
             // 
             // vendasBindingSource
@@ -120,14 +127,14 @@
             // textBox2
             // 
             this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.vendasBindingSource, "Endereco", true));
-            this.textBox2.Location = new System.Drawing.Point(429, 96);
+            this.textBox2.Location = new System.Drawing.Point(46, 165);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(166, 22);
+            this.textBox2.Size = new System.Drawing.Size(437, 22);
             this.textBox2.TabIndex = 15;
             // 
             // buttonBuscarCliente
             // 
-            this.buttonBuscarCliente.Location = new System.Drawing.Point(255, 93);
+            this.buttonBuscarCliente.Location = new System.Drawing.Point(250, 102);
             this.buttonBuscarCliente.Name = "buttonBuscarCliente";
             this.buttonBuscarCliente.Size = new System.Drawing.Size(29, 23);
             this.buttonBuscarCliente.TabIndex = 17;
@@ -138,14 +145,14 @@
             // nomeTextBox
             // 
             this.nomeTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.vendasBindingSource, "NomeCliente", true));
-            this.nomeTextBox.Location = new System.Drawing.Point(88, 93);
+            this.nomeTextBox.Location = new System.Drawing.Point(46, 103);
             this.nomeTextBox.Name = "nomeTextBox";
-            this.nomeTextBox.Size = new System.Drawing.Size(166, 22);
+            this.nomeTextBox.Size = new System.Drawing.Size(198, 22);
             this.nomeTextBox.TabIndex = 18;
             // 
             // buttonBuscarCorretor
             // 
-            this.buttonBuscarCorretor.Location = new System.Drawing.Point(255, 172);
+            this.buttonBuscarCorretor.Location = new System.Drawing.Point(489, 103);
             this.buttonBuscarCorretor.Name = "buttonBuscarCorretor";
             this.buttonBuscarCorretor.Size = new System.Drawing.Size(29, 23);
             this.buttonBuscarCorretor.TabIndex = 19;
@@ -155,7 +162,7 @@
             // 
             // buttonBuscarTerreno
             // 
-            this.buttonBuscarTerreno.Location = new System.Drawing.Point(601, 96);
+            this.buttonBuscarTerreno.Location = new System.Drawing.Point(489, 164);
             this.buttonBuscarTerreno.Name = "buttonBuscarTerreno";
             this.buttonBuscarTerreno.Size = new System.Drawing.Size(29, 23);
             this.buttonBuscarTerreno.TabIndex = 20;
@@ -163,11 +170,22 @@
             this.buttonBuscarTerreno.UseVisualStyleBackColor = true;
             this.buttonBuscarTerreno.Click += new System.EventHandler(this.buttonBuscarTerreno_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::LocacaoLaboratorio.Properties.Resources.Foto_de_fundo_2;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(579, 279);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
             // FormCadastroVendas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 270);
+            this.ClientSize = new System.Drawing.Size(579, 279);
             this.Controls.Add(this.buttonBuscarTerreno);
             this.Controls.Add(this.buttonBuscarCorretor);
             this.Controls.Add(nomeLabel);
@@ -180,10 +198,13 @@
             this.Controls.Add(this.labelCadastroVendas);
             this.Controls.Add(Terreno);
             this.Controls.Add(iDCorretorLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormCadastroVendas";
             this.Text = "FormCadastroVendas";
             this.Load += new System.EventHandler(this.FormCadastroVendas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.vendasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,5 +222,6 @@
         private System.Windows.Forms.TextBox nomeTextBox;
         private System.Windows.Forms.Button buttonBuscarCorretor;
         private System.Windows.Forms.Button buttonBuscarTerreno;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
